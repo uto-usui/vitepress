a0f9bd96887b31109ba1e42ed17ae4f9026b42bf
uto-usui <nattyanoranged@gmail.com>
2021-02-10 01:13:45 +0900

    💄 Add global style component

---
+++ docs/.vitepress/theme/Style.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="style">this is style component</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Style',
+})
+</script>
+
+<style>
+:root {
+  --c-brand: #ff6473;
+  --c-brand-light: #ff808c;
+
+  --c-white: #fafafa;
+  --c-black: #2a2828;
+}
+</style>
